86a1883e6850f3406a9022473a025d4134873f18
Steve Babb <steve.babb@outlook.com>
2021-03-01 12:04:35 -0600

    validate database db connection/response

--- dbQueries/queriesSQL.js
+++ dbQueries/queriesSQL.js
@@ -1,35 +1,36 @@
-const mysql = require('mysql');
-const inquirer = require('inquirer');
-const cTable = require('console.table');
-// const queriesSQL = require('./dbQueries/queriesSQL');
 
-// create the connection information for the sql database
+//NOT YET FUNCTIONAL
+
+const mysql = require('mysql');
+
 const connection = mysql.createConnection({
     host: 'localhost',
 
+    // Your port; if not 3306
     port: 3306,
 
+    // Your username
     user: 'root',
 
+    // Your password
     password: 'MySQL#4355',
     database: 'employee_trackerDB',
 });
 
 const readEmployees = () => {
-    console.log('Selecting all products...\n');
+    console.log('Selecting all employees...\n');
     connection.query('SELECT * FROM employee', (err, res) => {
         if (err) throw err;
-
+        // Log all results of the SELECT statement
         console.table(res);
         connection.end();
     });
-};
+}
 
-// connect to the mysql server and sql database
 connection.connect((err) => {
     if (err) throw err;
     console.log(`connected as id ${connection.threadId}\n`);
-    readEmployees();
 
 });
 
+module.exports = (readEmployees);
